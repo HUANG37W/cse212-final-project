f1cc1ee72d3ffaeb4e8371b70d2d9985be53e7ae
huangresume <huang78@byui.edu>
2022-06-29 10:42:23 -0600

    new changes

--- Solution.py
+++ Solution.py
@@ -1,26 +1,26 @@
 class TreeNode(object):
- def __init__(self, x):
-    self.val = x
-    self.left = None
-    self.right = None
+   def __init__(self, x):
+      self.val = x
+      self.left = None
+      self.right = None
 
     
 def array_to_bst(array_nums):
- if not array_nums:
-    return None
- mid_num = len(array_nums)//2
- node = TreeNode(array_nums[mid_num])
- node.left = array_to_bst(array_nums[:mid_num])
- node.right = array_to_bst(array_nums[mid_num+1:])
- return node
+   if not array_nums:
+      return None
+   mid_num = len(array_nums)//2
+   node = TreeNode(array_nums[mid_num])
+   node.left = array_to_bst(array_nums[:mid_num])
+   node.right = array_to_bst(array_nums[mid_num+1:])
+   return node
 
 
 def preOrder(node):
- if not node:
-    return
- print(node.val)
- preOrder(node.left)
- preOrder(node.right)
+   if not node:
+      return
+   print(node.val)
+   preOrder(node.left)
+   preOrder(node.right)
 
 
 array_nums = [1, 2, 3, 4, 5, 6, 7]
